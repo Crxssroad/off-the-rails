acb061ae9b60407b33989256286e5f26ebc8d823
kennie-create <kennieboulin1989@gmail.com>
2020-01-27 15:06:23 -0500

    Add test for viewing parks on index page

--- spec/features/user_sees_list_of_parks_spec.rb
+++ spec/features/user_sees_list_of_parks_spec.rb
@@ -1,8 +1,19 @@
 require 'rails_helper'
 
 feature 'user views all theme parks' do
+  before(:each) do
+    @mouse_house = Park.create(name:"Magic Kingdom", description:"Happiest place on earth")
+    @globe_house = Park.create(name:"Universal Studios", description:"Second happiest place on earth")
+  end
   scenario 'user views the homepage' do
     visit '/'
     expect(page).to have_content('Welcome to the Park\'s index')
+
+    expect(page).to have_content(@mouse_house.name)
+    expect(page).to have_content(@globe_house.name)
+
+    expect(page).to have_link(@mouse_house.name)
+    expect(page).to have_link(@globe_house.name)
+
   end
 end
