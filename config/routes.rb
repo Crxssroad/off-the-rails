a3d3442c9b31e2779a6fb47bada431296b5360fc
kennie-create <kennieboulin1989@gmail.com>
2020-01-27 15:37:01 -0500

    Modify routes to static pages to prepare for react

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  root 'parks#index'
+  root 'static_pages#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :parks, only: [:index, :show]
+  get '/parks', to: 'static_pages#index'
+  get '/parks/:id', to: 'static_pages#index'
 end
