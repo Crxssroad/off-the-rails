baeffdbb022860394c970f8d1857345c7d79001c
kennie-create <kennieboulin1989@gmail.com>
2020-01-27 16:28:56 -0500

    Setting up fetch request to parks api

--- app/javascript/react/components/ParksIndexContainer.js
+++ app/javascript/react/components/ParksIndexContainer.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react'
 
 const ParksIndexContainer = () => {
+  const [parks,setParks] = useState([])
+
+  useEffect(() =>{
+    fetch("/api/v1/parks")
+    .then(response => {
+      if(response.ok) {
+        return response
+      } else {
+        throw new Error(`${response.status}: ${response.statusText}`)
+      }
+    })
+    .then(validatedResponse => validatedResponse.json())
+    .then(body => {
+      console.log(body)
+      setParks(body.parks)
+    })
+    .catch(error => {
+      console.log(`Error fetching parks list ${error.message}`)
+    })
+  }, [])
+
+  const parkTiles = parks.map(park => {
+    return (
+      <li>
+        <p> {park.name} </p>
+        <p> {park.description} </p>
+      </li>
+    )
+  })
+
   return(
     <div>
-      <h1>Hi I'm the index container</h1>
+      <h1>Parks Index</h1>
+      <ul>{parkTiles}</ul>
     </div>
   )
 }
