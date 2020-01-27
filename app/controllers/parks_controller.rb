90c349e64b29a602b9c21c6960d131d4ffebfa43
kennie-create <kennieboulin1989@gmail.com>
2020-01-27 15:08:23 -0500

    Add link pointing to park show page

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -4,4 +4,8 @@ class ParksController < ApplicationController
     @parks = Park.all
   end
 
+  def show
+    @park = Park.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :parks, only: [:index]
+  resources :parks, only: [:index, :show]
 end
