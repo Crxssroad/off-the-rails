c470b4e44670ab799d8765fae9434f9f356073f2
dev-fildes <devafildes@gmail.com>
2020-01-27 16:26:34 -0500

    Enzyme testing.

--- spec/javascript/support/enzyme.js
+++ spec/javascript/support/enzyme.js
@@ -1,4 +1,15 @@
 const Enzyme = require('enzyme');
 const EnzymeAdapter = require("enzyme-adapter-react-16");
 
-Enzyme.configure({ adapter: new EnzymeAdapter() });
+import React from "react"
+import Enzyme, { mount } from "enzyme"
+import Adapter from "enzyme-adapter-react-16"
+import { BrowserRouter } from 'react-router-dom'
+
+Enzyme.configure({ adapter: new Adapter() })
+
+describe("example", () => {
+  it('should not print the error list', () => {
+    expect(true).toBe(true)
+  })
+})
