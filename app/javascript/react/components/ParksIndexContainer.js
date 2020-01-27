9f2712345653c1a996c2e9646a270bd830b49263
kennie-create <kennieboulin1989@gmail.com>
2020-01-27 15:39:24 -0500

    Set up react components for index and show

---
+++ app/javascript/react/components/ParksIndexContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ParksIndexContainer = () => {
+  return(
+    <div>
+      <h1>Hi I'm the index container</h1>
+    </div>
+  )
+}
+
+export default ParksIndexContainer
---
+++ app/javascript/react/components/ParksShowContainer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const ParksShowContainer = () => {
+  return(
+    <div>
+    </div>
+  )
+}
+
+export default ParksShowContainer
