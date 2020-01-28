e8b665a79c167dad723ec00070daddee32a31c2c
Crxssroad <gaby_encar28@hotmail.com>
2020-01-28 10:32:00 -0500

    Added Nickname field to our signup view and tested for it.

--- spec/features/user_signs_up_spec.rb
+++ spec/features/user_signs_up_spec.rb
@@ -11,6 +11,7 @@ feature 'user registers', %Q{
 
     fill_in 'First Name', with: 'Gabe'
     fill_in 'Last Name', with: 'Avacado'
+    fill_in 'Nickname', with: 'Cilantro'
     fill_in 'Email', with: 'john@example.com'
     fill_in 'Password', with: 'password'
     fill_in 'Password confirmation', with: 'password'
