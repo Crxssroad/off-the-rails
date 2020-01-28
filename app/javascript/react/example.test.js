db8c21d6439d0807bf7935b1a52004d304590de4
dev-fildes <devafildes@gmail.com>
2020-01-28 09:49:39 -0500

    Updated the files to create a space in the example.test file and removed admin methods and verification until we begin to implment admin function/features.

--- app/javascript/react/example.test.js
+++ app/javascript/react/example.test.js
@@ -2,6 +2,7 @@ import React from "react"
 import Enzyme, { mount } from "enzyme"
 import Adapter from "enzyme-adapter-react-16"
 import { BrowserRouter } from 'react-router-dom'
+
 Enzyme.configure({ adapter: new Adapter() })
 describe("example", () => {
   it('should not print the error list', () => {
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :role, presence: true, inclusion: { in: %w(member admin)}
 
-  def admin?
-    role == "admin"
-  end
-  
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :validatable
 end
