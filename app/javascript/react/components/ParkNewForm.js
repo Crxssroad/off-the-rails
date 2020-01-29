90c8b16f8799b947856851350393095c5a064faa
Braden Lawrence <b@bradenlawrence.com>
2020-01-29 13:19:17 -0500

    Added New Park Container and New Park Form

---
+++ app/javascript/react/components/ParkNewForm.js
@@ -0,0 +1,81 @@
+import React, { useState } from 'react'
+import { Redirect } from 'react-router-dom'
+
+const ParkNewForm = (props) => {
+  const defaultPark = {
+    name: "",
+    description: ""
+  }
+  const [park, setPark] = useState(defaultPark)
+  const [errors, setErrors] = useState([])
+
+  const handleInput = (event) => {
+    setPark({
+      ...park,
+      [event.currentTarget.name]: event.currentTarget.value
+    })
+  }
+
+  const errorList = () => {
+    return errors.map((error, index) => {
+      return(
+        <li key={index}>{error}</li>
+      )
+    })
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    fetch("/api/v1/parks", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(park)
+    })
+    .then(response => {
+      if(response.ok) {
+        return response.json()
+      } else {
+        throw new Error(`${response.status}: ${response.statusText}`)
+      }
+    })
+    .then(parsedBody => {
+      if(typeof parsedBody === "object") {
+        return <Redirect to={`/parks/${parsedBody.id}`} />
+      } else {
+        setErrors(parsedBody)
+      }
+    })
+    .catch(error => console.log(`Error posting park ${error.message}`))
+  }
+
+  return(
+    <div>
+      <ul>
+        {errorList}
+      </ul>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name:
+          <input
+            name="name"
+            onChange={handleInput}
+            value={park.name}
+          />
+        </label>
+        <label>
+          Description:
+          <input
+            name="description"
+            onChange={handleInput}
+            value={park.description}
+          />
+        </label>
+        <input type="submit" value="Add new park" />
+      </form>
+    </div>
+  )
+}
+
+export default ParkNewForm
