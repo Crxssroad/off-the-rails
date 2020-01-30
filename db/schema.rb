223c8419dffec9f4d2362ad06e601a03b9cfa177
Crxssroad <gaby_encar28@hotmail.com>
2020-01-30 12:24:37 -0500

    Created tags table and tag model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_27_192449) do
+ActiveRecord::Schema.define(version: 2020_01_30_172121) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,20 @@ ActiveRecord::Schema.define(version: 2020_01_27_192449) do
   create_table "parks", force: :cascade do |t|
     t.string "name", null: false
     t.text "description", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "body", null: false
+    t.integer "rating", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -34,6 +48,7 @@ ActiveRecord::Schema.define(version: 2020_01_27_192449) do
     t.string "last_name", null: false
     t.string "role", default: "member", null: false
     t.string "nickname"
+    t.string "profile_photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
