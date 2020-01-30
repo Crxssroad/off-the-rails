05994d01d37ca1f7b1d652b903690282cab5f5c6
kennie-create <kennieboulin1989@gmail.com>
2020-01-30 16:03:06 -0500

    Updated review model test

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Review, type: :model do
+  it { should belong_to :park }
+
   it { should have_valid(:title).when("Had a great time") }
   it { should have_valid(:body).when("Because Braden was there") }
   it { should have_valid(:rating).when(4) }
