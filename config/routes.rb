84e34bb3cae8bbc3906b4a1339398c8193d9e6f9
Braden Lawrence <b@bradenlawrence.com>
2020-01-30 11:34:39 -0500

    Fixed indentation

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   namespace "api" do
     namespace "v1" do
       resources :parks, only: [:index, :show, :create, :new] do
-      resources :reviews, only: [:index, :new, :create]
+        resources :reviews, only: [:index, :new, :create]
       end
     end
   end
