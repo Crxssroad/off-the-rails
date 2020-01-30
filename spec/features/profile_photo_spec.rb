615208c509dc9b746b9a80420da44bc6a3419b83
Crxssroad <gaby_encar28@hotmail.com>
2020-01-30 10:20:44 -0500

    Added testing for profile photo uploads.

---
+++ spec/features/profile_photo_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+feature "profile photo" do
+  scenario "user uploads a profile photo" do
+    visit root_path
+    click_link "Sign Up"
+
+    fill_in 'First Name', with: 'Mr.'
+    fill_in 'Last Name', with: 'Boomer'
+    fill_in "Email", with: "ash@s-mart.com"
+    fill_in "Password", with: "boomstick!3vilisd3ad"
+    fill_in "Password confirmation", with: "boomstick!3vilisd3ad"
+    attach_file :user_profile_photo, "#{Rails.root}/spec/support/images/photo.png"
+    click_button "Sign up"
+
+    expect(page).to have_content("Welcome! You have signed up successfully.")
+  end
+end
