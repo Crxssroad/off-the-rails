e10ff1ed85a15a9588e7b5cc4eed30cfb3304758
kennie-create <kennieboulin1989@gmail.com>
2020-01-30 15:56:56 -0500

    Created model test for Review

---
+++ spec/models/review_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe Review, type: :model do
+  it { should have_valid(:title).when("Had a great time") }
+  it { should have_valid(:body).when("Because Braden was there") }
+  it { should have_valid(:rating).when(4) }
+
+  it { should_not have_valid(:title).when(nil, "") }
+  it { should_not have_valid(:body).when(nil, "") }
+  it { should_not have_valid(:rating).when(-1, 5000, 3.14) }
+end
