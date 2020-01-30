14bda4536c38c58837c2ced8a5891fa8517a08cb
Braden Lawrence <b@bradenlawrence.com>
2020-01-30 12:12:14 -0500

    Removed unneeded :new route for reviews

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   namespace "api" do
     namespace "v1" do
       resources :parks, only: [:index, :show, :create, :new] do
-        resources :reviews, only: [:index, :new, :create]
+        resources :reviews, only: [:index, :create]
       end
     end
   end
