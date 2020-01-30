218397922b91f924ea79a46c47d7b1798525013e
Braden Lawrence <b@bradenlawrence.com>
2020-01-30 11:36:02 -0500

    Added number validations for rating

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,5 +3,9 @@ class Review < ApplicationRecord
 
   validates :title, presence: true
   validates :body, presence: true
-  validates :rating, presence: true
+  validates :rating, presence: true, numericality: {
+    greater_than_or_equal_to: 1,
+    less_than_or_equal_to: 5,
+    only_integer: true  
+  }
 end
