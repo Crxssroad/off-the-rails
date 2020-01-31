87caf3451c4876149d30c59b0e235592239774ed
Crxssroad <gaby_encar28@hotmail.com>
2020-01-31 12:21:46 -0500

    Added display name to review tile and destructured props.

--- app/javascript/react/components/ReviewTile.js
+++ app/javascript/react/components/ReviewTile.js
@@ -1,11 +1,15 @@
 import React from 'react'
 
-const ReviewTile = (props) => {
+const ReviewTile = ({ title, body, rating, user }) => {
+  let displayName = user.nickname !== ""
+    ? user.nickname
+    : `${user.first_name} ${user.last_name}`
   return(
     <div>
-      <h3>{props.title}</h3>
-      <p>{props.body}</p>
-      <span>{props.rating}</span>
+      <h3>{title}</h3>
+      <span>{displayName}</span>
+      <p>{body}</p>
+      <span>{rating}</span>
     </div>
   )
 }
