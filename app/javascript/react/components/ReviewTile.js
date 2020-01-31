dbc29eba8991120cf905fa14d43284433d043e89
Crxssroad <gaby_encar28@hotmail.com>
2020-01-31 12:32:48 -0500

    Added User serializer and custom display_name attribute.

--- app/javascript/react/components/ReviewTile.js
+++ app/javascript/react/components/ReviewTile.js
@@ -1,13 +1,11 @@
 import React from 'react'
 
 const ReviewTile = ({ title, body, rating, user }) => {
-  let displayName = user.nickname !== ""
-    ? user.nickname
-    : `${user.first_name} ${user.last_name}`
+
   return(
     <div>
       <h3>{title}</h3>
-      <span>{displayName}</span>
+      <span>{user.display_name}</span>
       <p>{body}</p>
       <span>{rating}</span>
     </div>
