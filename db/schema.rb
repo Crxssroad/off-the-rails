c79e152b27d1f6a5f94c02139f675984b1b8700b
Crxssroad <gaby_encar28@hotmail.com>
2020-01-31 10:14:27 -0500

    Added a reference to users from reviews

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_30_143951) do
+ActiveRecord::Schema.define(version: 2020_01_31_144905) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 2020_01_30_143951) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "park_id", null: false
+    t.bigint "user_id", default: 1, null: false
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
