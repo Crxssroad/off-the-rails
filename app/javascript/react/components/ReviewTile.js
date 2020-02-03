802fee3c63ff896634fda6d4084b911af96e530a
Crxssroad <gaby_encar28@hotmail.com>
2020-02-03 14:42:10 -0500

    Created saveReview and deleteReview functions to post to ReviewController actions, added logic to display Edit and Delete buttons. Added logic to display review form

--- app/javascript/react/components/ReviewTile.js
+++ app/javascript/react/components/ReviewTile.js
@@ -1,23 +1,115 @@
-import React from 'react'
+import React, { useState, Fragment } from 'react'
 
-const ReviewTile = ({ title, body, rating, user, signedInUser }) => {
+import ReviewForm from './ReviewForm'
+import ErrorList from './ErrorList'
+
+const ReviewTile = ({ review, user, signedInUser, parkId }) => {
+  let { title, body, rating, id } = review
+  const [tileReview, setTileReview] = useState(review)
+  const [editClicked, setEditClicked] = useState(false)
+  const [errors, setErrors] = useState([])
+  const [deleted, setDeleted] = useState(false)
+
+  const onEditClick = event => {
+    event.preventDefault()
+    setEditClicked(true)
+  }
+
+  const saveReview = (formPayload) => {
+    fetch(`/api/v1/parks/${parkId}/reviews/${review.id}`, {
+      credentials: 'same-origin',
+      method: "PATCH",
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify(formPayload)
+    })
+    .then(response => {
+      if (response.ok) {
+        return response.json()
+      } else {
+        throw new Error(`${response.status} ${response.statusText}`)
+      }
+    })
+    .then(parsedBody => {
+      if (typeof parsedBody === "object" && !Array.isArray(parsedBody)) {
+        setTileReview(parsedBody.review)
+        setEditClicked(false)
+        setErrors([])
+      } else {
+        setErrors(parsedBody)
+      }
+    })
+    .catch(error => console.error(`Error in fetch ${error.message}`))
+  }
+
+  const deleteReview = () => {
+    fetch(`/api/v1/parks/${parkId}/reviews/${review.id}`, {
+      credentials: 'same-origin',
+      method: "DELETE",
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      }
+    })
+    .then(response => {
+      if (response.ok) {
+        return response.json()
+      } else {
+        throw new Error(`${response.status} ${response.statusText}`)
+      }
+    })
+    .then(parsedBody => {
+      setDeleted(true)
+    })
+    .catch(error => console.error(`Error in fetch ${error.message}`))
+  }
+
   let updateDeleteButtons
 
   if (signedInUser && signedInUser.id === user.id || signedInUser && signedInUser.role === "admin") {
     updateDeleteButtons = <form>
-      <input type="button" value="Edit" />
-      <input type="button" value="Delete" />
+      <input onClick={onEditClick} type="button" value="Edit" />
+      <input onClick={deleteReview} type="button" value="Delete" />
     </form>
   }
 
-  return(
+  let display =
     <div>
-      <h3>{title}</h3>
+      <h3>{tileReview.title}</h3>
       <span className="author">{user.display_name}</span>
-      <p>{body}</p>
-      <span className="rating">{rating}</span>
+      <p>{tileReview.body}</p>
+      <span className="rating">{tileReview.rating}</span>
       {updateDeleteButtons}
     </div>
+
+    if (editClicked) {
+      let errorList
+      if (errors.length > 0) {
+        errorList = <ErrorList errors={errors} />
+      }
+      const cancelClicked = () => {
+        setEditClicked(false)
+      }
+      display =
+      <div>
+        {errorList}
+        <ReviewForm
+          editReview={review}
+          saveReview={saveReview}
+          cancelClicked={cancelClicked}
+        />
+      </div>
+    }
+
+    if(deleted){
+      display = "Review successfully deleted"
+    }
+  return(
+    <Fragment>
+      {display}
+    </Fragment>
   )
 }
 
