09e12a5297f24b065137f2f66eb90cbb4f1fde04
Crxssroad <gaby_encar28@hotmail.com>
2020-02-04 15:03:25 -0500

    Added logic to display rating or "Be the first to review" if there are no ratings.

--- app/javascript/react/components/ParkDetailTile.js
+++ app/javascript/react/components/ParkDetailTile.js
@@ -1,9 +1,17 @@
 import React from 'react'
 
 const ParkDetailTile = (props) => {
+
+  let ratingDisplay = props.averageRating
+
+  if (props.averageRating === 0) {
+    ratingDisplay = "Be the first to review"
+  }
+
   return (
     <div>
       <h2>{props.name}</h2>
+      {ratingDisplay}
       <p>{props.description}</p>
     </div>
   )
