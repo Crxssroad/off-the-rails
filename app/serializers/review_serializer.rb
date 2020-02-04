d517b43f6ab61d86042a23c167ebf3478578dc0f
Crxssroad <gaby_encar28@hotmail.com>
2020-02-04 15:15:12 -0500

    Added park association to review serializer.

--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -2,6 +2,7 @@ class ReviewSerializer < ActiveModel::Serializer
   attributes :id, :title, :body, :rating, :user, :display_name
 
   belongs_to :user, serializer: UserSerializer
+  belongs_to :park, serializer: ParkSerializer
 
   def display_name
     object.user.nickname != "" ? object.user.nickname : "#{object.user.first_name} #{object.user.last_name}"
