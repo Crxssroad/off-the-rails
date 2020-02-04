b6ca1d077dc5c7703474528a23460efbb3671f09
Crxssroad <gaby_encar28@hotmail.com>
2020-02-04 14:46:32 -0500

    Modified park serializer to also serve up currently signed in user.

--- app/serializers/park_serializer.rb
+++ app/serializers/park_serializer.rb
@@ -1,5 +1,13 @@
 class ParkSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :reviews
+  attributes :id, :name, :description, :reviews, :average_rating, :currentUser
 
-  has_many :reviews
+  has_many :reviews, each_serializer: ReviewSerializer
+
+  def average_rating
+    object.total_rating / (object.reviews.length > 0 ? object.reviews.length : 1)
+  end
+
+  def currentUser
+    current_user
+  end
 end
