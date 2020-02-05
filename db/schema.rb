a359d92e2e64d616f063ba485132329e9696af75
kennie-create <kennieboulin1989@gmail.com>
2020-02-05 15:12:18 -0500

    Merged

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_30_172602) do
+ActiveRecord::Schema.define(version: 2020_02_05_150728) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,10 @@ ActiveRecord::Schema.define(version: 2020_01_30_172602) do
     t.text "description", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "total_rating", default: 0, null: false
+    t.string "city", null: false
+    t.string "state"
+    t.string "country", null: false
   end
 
   create_table "parks_tags", force: :cascade do |t|
@@ -38,6 +42,8 @@ ActiveRecord::Schema.define(version: 2020_01_30_172602) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "park_id", null: false
+    t.bigint "user_id", default: 1, null: false
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "tags", force: :cascade do |t|
