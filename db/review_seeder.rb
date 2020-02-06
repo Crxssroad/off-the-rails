576c658d25ef7a8864f49523dfcd640d694aded8
Braden Lawrence <b@bradenlawrence.com>
2020-02-06 16:31:22 -0500

    Increased the number of seed reviews to give each park a better chance of having more than one

--- db/review_seeder.rb
+++ db/review_seeder.rb
@@ -109,6 +109,111 @@ class ReviewSeeder
         body: LONG_REVIEW,
         user_id: users.sample.id,
         park_id: parks.sample.id
+      },
+      {
+        title: "It was okay.",
+        rating: 3,
+        body: SHORT_REVIEW,
+        user_id: users.sample.id,
+        park_id: parks.sample.id
+      },
+      {
+        title: "It was great!",
+        rating: 5,
+        body: SHORT_REVIEW,
+        user_id: users.sample.id,
+        park_id: parks.sample.id
+      },
+      {
+        title: "It was awful!",
+        rating: 1,
+        body: SHORT_REVIEW,
+        user_id: users.sample.id,
+        park_id: parks.sample.id
+      },
+      {
+        title: "I got lost",
+        rating: 2,
+        body: MEDIUM_REVIEW,
+        user_id: users.sample.id,
+        park_id: parks.sample.id
+      },
+      {
+        title: "I lost my kid!",
+        rating: 2,
+        body: MEDIUM_REVIEW,
+        user_id: users.sample.id,
+        park_id: parks.sample.id
+      },
+      {
+        title: "Food was great!",
+        rating: 4,
+        body: MEDIUM_REVIEW,
+        user_id: users.sample.id,
+        park_id: parks.sample.id
+      },
+      {
+        title: "Food was okay!",
+        rating: 3,
+        body: LONG_REVIEW,
+        user_id: users.sample.id,
+        park_id: parks.sample.id
+      },
+      {
+        title: "Worst rollercoasters ever!!!",
+        rating: 1,
+        body: LONG_REVIEW,
+        user_id: users.sample.id,
+        park_id: parks.sample.id
+      },
+      {
+        title: "Okayest rollercoasters ever!!!",
+        rating: 3,
+        body: MEDIUM_REVIEW,
+        user_id: users.sample.id,
+        park_id: parks.sample.id
+      },
+      {
+        title: "Best cotton candy ever!!!",
+        rating: 5,
+        body: LONG_REVIEW,
+        user_id: users.sample.id,
+        park_id: parks.sample.id
+      },
+      {
+        title: "Worst cotton candy ever!!!",
+        rating: 1,
+        body: SHORT_REVIEW,
+        user_id: users.sample.id,
+        park_id: parks.sample.id
+      },
+      {
+        title: "The clown traumatized me!",
+        rating: 1,
+        body: LONG_REVIEW,
+        user_id: users.sample.id,
+        park_id: parks.sample.id
+      },
+      {
+        title: "I certainly was there",
+        rating: 3,
+        body: MEDIUM_REVIEW,
+        user_id: users.sample.id,
+        park_id: parks.sample.id
+      },
+      {
+        title: "I think I had fun",
+        rating: 5,
+        body: MEDIUM_REVIEW,
+        user_id: users.sample.id,
+        park_id: parks.sample.id
+      },
+      {
+        title: "Lifelong memory!",
+        rating: 5,
+        body: LONG_REVIEW,
+        user_id: users.sample.id,
+        park_id: parks.sample.id
       }
     ]
   end
