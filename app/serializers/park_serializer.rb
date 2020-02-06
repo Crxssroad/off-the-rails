78d4345ad85b90c2c0c4618f4ea922b29e003dc0
Crxssroad <gaby_encar28@hotmail.com>
2020-02-06 10:15:23 -0500

    Sorted order of reviews from newest to oldest on Park serializer.

--- app/serializers/park_serializer.rb
+++ app/serializers/park_serializer.rb
@@ -1,7 +1,11 @@
 class ParkSerializer < ActiveModel::Serializer
   attributes :id, :name, :description, :reviews, :average_rating, :currentUser
 
-  has_many :reviews, each_serializer: ReviewSerializer
+  has_many :reviews
+
+  def reviews
+    object.reviews.order('created_at DESC')
+  end
 
   def average_rating
     object.total_rating / (object.reviews.length > 0 ? object.reviews.length : 1)
