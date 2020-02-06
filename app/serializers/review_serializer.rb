546945656c1ab30c929e3328c7689c740b25a60b
Crxssroad <gaby_encar28@hotmail.com>
2020-02-06 10:16:26 -0500

    Added logic to display upvote/downvote buttons depending on user authentication state.

--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,10 +1,18 @@
 class ReviewSerializer < ActiveModel::Serializer
-  attributes :id, :title, :body, :rating, :user, :display_name
+  attributes :id, :title, :body, :rating, :user, :display_name, :vote_count, :vote_status
 
   belongs_to :user, serializer: UserSerializer
   belongs_to :park, serializer: ParkSerializer
 
   def display_name
     object.user.nickname != "" ? object.user.nickname : "#{object.user.first_name} #{object.user.last_name}"
+  end
+
+  def vote_status
+    vote = nil
+    if current_user
+      vote = Vote.find_by(user_id: current_user.id, review: object.id)
+    end
+    vote
   end
 end
