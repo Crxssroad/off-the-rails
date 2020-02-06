f55d9a447e928d1c81d9acb50224376f313970d8
Crxssroad <gaby_encar28@hotmail.com>
2020-02-06 11:22:52 -0500

    Added default values to null false fields on migration.

--- db/migrate/20200205150728_create_park_locations.rb
+++ db/migrate/20200205150728_create_park_locations.rb
@@ -1,7 +1,7 @@
 class CreateParkLocations < ActiveRecord::Migration[5.2]
   def change
-      add_column :parks, :city, :string, null: false
+      add_column :parks, :city, :string, null: false, default: "To Be Added"
       add_column :parks, :state, :string
-      add_column :parks, :country, :string, null: false
+      add_column :parks, :country, :string, null: false, default: "To Be Added"
     end
   end
